2631c83475c8eca99ed055a7a54342ebde383bc4
ayydwii <dw717748@gmail.com>
2025-09-23 06:04:52 +0700

    Tugas2 route CRUD selesai

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,20 +8,33 @@
 <body>
     <h1>Daftar Post</h1>
 
-    <table border="1" cellpadding="8">
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Content</th>
-        </tr>
+    <a href="{{ route('posts.create') }}">Tambah Post</a>
+
+        <table border="1" cellpadding="8">
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Aksi</th>
+            </tr>
 
         @foreach ($posts as $post)
             <tr>
                 <td>{{ $post->id }}</td>
                 <td>{{ $post->title }}</td>
                 <td>{{ $post->content }}</td>
+                <td>
+                    <a href="{{ route('posts.show', $post->id) }}">Lihat</a> |
+                    <a href="{{ route('posts.edit', $post->id) }}">Edit</a> |
+                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" onclick="return confirm('Yakin hapus data ini?')">Hapus</button>
+                    </form>
+                </td>
             </tr>
         @endforeach
-    </table>
+</table>
+
 </body>
 </html>

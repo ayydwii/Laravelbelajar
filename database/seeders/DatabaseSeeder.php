e08490e86199a6010cc30a4efcb6c83f7351ab5f
ayydwii <dw717748@gmail.com>
2025-10-02 22:25:13 +0700

    perbaikan DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'username' => 'testuser',
             'email' => 'test@example.com',
+            'password' => 'password', // password
         ]);
     }
 }
